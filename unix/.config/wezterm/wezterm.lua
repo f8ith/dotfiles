18a1f2b2867b0df4c279cec2d740de337aad4e90
faith <jamienibtong@gmail.com>
2024-04-14 15:41:27 +0800

    added windows config

--- unix/.config/wezterm/wezterm.lua
+++ unix/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm';
+local config = wezterm.config_builder()
 
 local ayu_colors = {
     bg = "#e7e8e9",
@@ -19,45 +20,49 @@ local one_dark_colors = {
     accent = "#C678DD"
 }
 
-local colors = one_dark_colors
+-- local colors = one_dark_colors
 
-return {
-    leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
-    font = wezterm.font { family = "Roboto Mono", harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, },
-    font_size = 14,
-    color_scheme = 'Catppuccin Mocha',
-    use_dead_keys = false,
-    use_fancy_tab_bar = false,
-    enable_wayland = true,
-    keys = {
-        { key = "a", mods = "LEADER|CTRL", action = wezterm.action { SendString = "\x01" } },
-        { key = "-", mods = "LEADER",      action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
-        {
-            key = "|",
-            mods = "LEADER|SHIFT",
-            action = wezterm.action { SplitHorizontal = {
-                domain = "CurrentPaneDomain" } }
-        },
-        { key = "z", mods = "LEADER",       action = "TogglePaneZoomState" },
-        { key = "c", mods = "LEADER",       action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
-        { key = "h", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Left" } },
-        { key = "j", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Down" } },
-        { key = "k", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Up" } },
-        { key = "l", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Right" } },
-        { key = "H", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
-        { key = "J", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
-        { key = "K", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
-        { key = "L", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
-        { key = "1", mods = "LEADER",       action = wezterm.action { ActivateTab = 0 } },
-        { key = "2", mods = "LEADER",       action = wezterm.action { ActivateTab = 1 } },
-        { key = "3", mods = "LEADER",       action = wezterm.action { ActivateTab = 2 } },
-        { key = "4", mods = "LEADER",       action = wezterm.action { ActivateTab = 3 } },
-        { key = "5", mods = "LEADER",       action = wezterm.action { ActivateTab = 4 } },
-        { key = "6", mods = "LEADER",       action = wezterm.action { ActivateTab = 5 } },
-        { key = "7", mods = "LEADER",       action = wezterm.action { ActivateTab = 6 } },
-        { key = "8", mods = "LEADER",       action = wezterm.action { ActivateTab = 7 } },
-        { key = "9", mods = "LEADER",       action = wezterm.action { ActivateTab = 8 } },
-        { key = "&", mods = "LEADER|SHIFT", action = wezterm.action { CloseCurrentTab = { confirm = true } } },
-        { key = "x", mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    config.default_prog = { "pwsh", "-NoLogo" }
+end
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.font = wezterm.font { family = "Roboto Mono", harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, }
+config.font_size = 14
+config.color_scheme = 'Catppuccin Mocha'
+config.use_dead_keys = false
+config.use_fancy_tab_bar = false
+config.enable_wayland = true
+config.keys = {
+    { key = "a", mods = "LEADER|CTRL", action = wezterm.action { SendString = "\x01" } },
+    { key = "-", mods = "LEADER",      action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+    {
+        key = "|",
+        mods = "LEADER|SHIFT",
+        action = wezterm.action { SplitHorizontal = {
+            domain = "CurrentPaneDomain" } }
     },
+    { key = "z", mods = "LEADER",       action = "TogglePaneZoomState" },
+    { key = "c", mods = "LEADER",       action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+    { key = "h", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Left" } },
+    { key = "j", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Down" } },
+    { key = "k", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Up" } },
+    { key = "l", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Right" } },
+    { key = "H", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+    { key = "J", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+    { key = "K", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+    { key = "L", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+    { key = "1", mods = "LEADER",       action = wezterm.action { ActivateTab = 0 } },
+    { key = "2", mods = "LEADER",       action = wezterm.action { ActivateTab = 1 } },
+    { key = "3", mods = "LEADER",       action = wezterm.action { ActivateTab = 2 } },
+    { key = "4", mods = "LEADER",       action = wezterm.action { ActivateTab = 3 } },
+    { key = "5", mods = "LEADER",       action = wezterm.action { ActivateTab = 4 } },
+    { key = "6", mods = "LEADER",       action = wezterm.action { ActivateTab = 5 } },
+    { key = "7", mods = "LEADER",       action = wezterm.action { ActivateTab = 6 } },
+    { key = "8", mods = "LEADER",       action = wezterm.action { ActivateTab = 7 } },
+    { key = "9", mods = "LEADER",       action = wezterm.action { ActivateTab = 8 } },
+    { key = "&", mods = "LEADER|SHIFT", action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+    { key = "x", mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
 }
+
+return config
---
+++ windows/Bootstrap.ps1
@@ -0,0 +1,81 @@
+# Setup script for Windots
+#Requires -RunAsAdministrator
+
+# Linked Files (Destination => Source)
+$symlinks = @{
+    $PROFILE.CurrentUserAllHosts                                                                    = ".\Profile.ps1"
+    "$HOME\AppData\Local\nvim"                                                                      = "..\unix\.config\nvim"
+    "$HOME\.gitconfig"                                                                              = ".\.gitconfig"
+    "$ENV:PROGRAMFILES\WezTerm\wezterm_modules"                                                     = "..\unix\.config\wezterm\"
+    "$HOME\.wezterm.lua"                                                                            = "..\unix\.config\wezterm\wezterm.lua"
+    "$HOME\.ideavimrc"                                                                              = "..\unix\.ideavimrc"
+}
+
+# Winget dependencies
+$wingetDeps = @(
+    "Eza-community.Eza"
+    "Git.Git"
+    "GitHub.Cli"
+    "Microsoft.OpenJDK.21"
+    "Microsoft.PowerShell"
+    "OpenJS.NodeJS"
+)
+
+# PS Modules
+$psModules = @(
+    "ps-color-scripts"
+    "PSScriptAnalyzer"
+    "CompletionPredictor"
+)
+
+# Set working directory
+Set-Location $PSScriptRoot
+[Environment]::CurrentDirectory = $PSScriptRoot
+
+Write-Host "Installing missing dependencies..."
+$installedWingetDeps = winget list | Out-String
+foreach ($wingetDep in $wingetDeps) {
+    if ($installedWingetDeps -notmatch $wingetDep) {
+        winget install -e --id $wingetDep
+    }
+}
+
+# Path Refresh
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+# Install PS Modules
+foreach ($psModule in $psModules) {
+    if (!(Get-Module -ListAvailable -Name $psModule)) {
+        Install-Module -Name $psModule -Force -AcceptLicense -Scope CurrentUser
+    }
+}
+
+# Create Custom NVIM shotcut
+# if (!(Test-Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\nvim.lnk")) {
+#     $wshShell = New-Object -ComObject WScript.Shell
+#     $shortcut = $wshShell.CreateShortcut("$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\nvim.lnk")
+#     $shortcut.TargetPath = "C:\tools\neovim\nvim-win64\bin\nvim.exe"
+#     $shortcut.workingDirectory = (Resolve-Path ..) # Set working directory to parent directory of this script (likely where you keep all Git Projects)
+#     $shortcut.IconLocation = "C:\tools\neovim\nvim-win64\bin\nvim-qt.exe,0" # Steal icon from nvim-qt.exe
+#     $shortcut.Save()
+# }
+
+# Delete OOTB Nvim Shortcuts (including QT)
+# if (Test-Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Neovim\") {
+#     Remove-Item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Neovim\" -Recurse -Force
+# }
+
+$currentGitEmail = (git config --global user.email)
+$currentGitName = (git config --global user.name)
+
+# Create Symbolic Links
+Write-Host "Creating Symbolic Links..."
+foreach ($symlink in $symlinks.GetEnumerator()) {
+    Get-Item -Path $symlink.Key -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+    New-Item -ItemType SymbolicLink -Path $symlink.Key -Target (Resolve-Path $symlink.Value) -Force | Out-Null
+}
+
+git config --global --unset user.email | Out-Null
+git config --global --unset user.name | Out-Null
+git config --global user.email $currentGitEmail | Out-Null
+git config --global user.name $currentGitName | Out-Null
